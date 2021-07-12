c660f9f323e9d52ad9b3d1f89629cc4135202003
Vamsi Krishna <mvkrishna2001@gmail.com>
2021-07-12 20:26:32 +0530

    Added a doc string for PriorityQueue

--- utils/CollectionUtils.py
+++ utils/CollectionUtils.py
@@ -1,6 +1,7 @@
 """Reducible Lazy Evaluation"""
 import heapq
 
+
 class ReducibleLazyEvaluation:
     def __init__(self, K, V):
         self.K = K
@@ -13,18 +14,17 @@ class ReducibleLazyEvaluation:
         dictionary[K] = V
         return dictionary
 
+
 class PriorityQueue:
+    """A max heap"""
+
     def __init__(self):
         self._data = []
         self._index = 0
+
     def push(self, item, priority):
         heapq.heappush(self._data, (-priority, self._index, item))
         self._index += 1
+
     def pop(self):
         return heapq.heappop(self._data)[-1]
-
-
-
-
-
-
