d706c65ed388bee88c30c045dd7b878dc1b68057
Sofia Alvarez <ms.alvarezl@uniandes.edu.co>
2021-08-10 17:41:10 -0500

    SingleLink implementation finally done

--- algorithm/SingleLink.py
+++ algorithm/SingleLink.py
@@ -1,9 +1,30 @@
 # It looks like single link is an instantiation of the OnlineAlgorithm class.
 from .OnlineAlgorithm import OnlineAlgorithm
 from ..topo.Topo import Topo
-from ..topo.Node import to
+from ..topo.Node import to,Edge
 
+
+# It has to be done a little differently in Python
+# Theoretically, it should work if we call any of the OnlineAlgorithm functions.
 class SingleLink(OnlineAlgorithm):
-    def __init__(self, topo, allowRecoveryPaths):
-        self.topo.links = topo.links
+    def __init__(self, topo, allowRecoveryPaths=True):
+        super().__init__(topo, allowRecoveryPaths=True)
+        prov = {}
+        self.topo.links = []
+        for link in topo.links:
+            edge = Edge(link.node1, link.node2)
+            if edge not in self.topo.links:
+                prov[edge] = [link]
+            else:
+                prov[edge].append(link)
+        for pair in prov:
+            first = prov[pair][0]
+            self.topo.links.append(first)
+        self.allowRecoveryPaths = allowRecoveryPaths
+        self.name = 'SL'.join('' if allowRecoveryPaths else '-R')
+
+
+
+
+
 
